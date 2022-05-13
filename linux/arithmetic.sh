824368fa04ba6f18554a910325137d88f46c7d30
salman-basha <87054429+salman-basha@users.noreply.github.com>
2022-05-13 11:18:55 +0530

    Add files via upload

---
+++ linux/arithmetic.sh
@@ -0,0 +1,14 @@
+x=8
+y=2
+echo "x=8,y=2"
+echo "Addition of 2 Number"
+echo $(($x + $y))
+echo "Subtraction of 2 Number"
+echo $(($x - $y))
+echo "Multiplication of 2 Number"
+echo $(($x * $y))
+echo "Division of 2 Number"
+echo $(($x / $y))
+echo "Remainder of Division"
+echo $(($x % $y))
+
